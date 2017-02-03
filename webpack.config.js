ea2358c7c9f641d6a0dd93fdd6b06097c26394c4
Rhys Gevaux <rhys.gevaux@blueq.com.au>
2017-02-03 12:39:49 +1100

    Working build

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+const { resolve } = require('path')
+
+const PATHS = {
+  app: resolve(__dirname, 'app'),
+  build: resolve(__dirname, 'dist'),
+}
+
+const outputPath = PATHS.build
+
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  filename: 'index.html',
+  inject: 'body',
+  template: `${PATHS.app}/index.html`,
+})
+
+module.exports = {
+  context: PATHS.app,
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: outputPath,
+    hot: true,
+    inline: true,
+    publicPath: '/',
+  },
+  entry: [
+    'react-hot-loader/patch',
+    'webpack-dev-server/client',
+    'webpack/hot/only-dev-server',
+    './index.jsx',
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: outputPath,
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: ['babel-loader'],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    HtmlWebpackPluginConfig,
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+  ],
+  resolve: {
+    enforceExtension: false,
+    extensions: ['.js', '.json', '.jsx'],
+    modules: [resolve('./app'), 'node_modules'],
+  },
+}
